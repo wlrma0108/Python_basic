819e097cf0cab4305a71bfee45ddd32938c0cbf1
wlrma0108 <dlwlrma0108@gmail.com>
2022-03-20 19:11:05 +0900

    commit

---
+++ List.py
@@ -0,0 +1,32 @@
+a=list()
+odd = [1,3,5,7,9]
+c=['life', 'of', 'py']
+d=['1','2','of',['life','dfs']]
+a=[1, 2, 3, ['a', 'b', 'c']]
+
+a[0]
+a[3][2]
+a[0:2]
+
+
+a+c
+a*3
+
+len(a)
+
+str(a[2])+"hi" 
+del a[1]
+
+a.append(4)
+a.append([3,1123])
+a.sort()
+a.reverse
+a.index(3)
+
+a.insert(3,1)
+
+a.remove(3)
+a.pop()
+a.count(1)
+a.extend([4,2])
+
---
+++ String.py
@@ -0,0 +1,58 @@
+"Life is too short, you need Python"
+"a"
+"123"
+
+
+"Hello world"
+'Python is fun'
+"""Life is too short you need python"""
+'''if you feel life is too short you need django'''
+
+food="Python's favorate food is perl"
+
+multiline = """
+Life is too short
+you need python
+"""
+
+a= "python"
+a*50
+print(a)
+
+a = "feture of the linux container"
+len(a)
+a[3]
+a[-2]
+
+b=a[0]+a[1]+a[2]+a[3]
+a[0:4]
+
+
+"I eat %d apples" %3 
+
+"I eat %d apples "  %"five"
+
+
+"I eat %s apples" %3
+
+"I eat %s apples" % 3.123e10
+
+"0.4f" % 3.43123
+"i eat {0} apples ". format(3)
+num=3 
+day=2
+
+"i ate {0} apples. so i was sick for {1} days.".format(num,day)
+"I ate {num} apples. so I was sick for {day} days".format(num=3,day=2)
+
+a='hobby'
+
+a.count('b')
+
+
+a ="Python is too simple"
+a.find()
+
+a.split()
+
+a.split(':')
